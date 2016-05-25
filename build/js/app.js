ae0a956512ace882b75e454753a4a0864512f5e4
Lilo Onwuzu <lpr422@gmail.com>
2016-05-25 10:01:28 -0700

    Alarm module works. Snooze modules works. Trouble shoot snooze to stop blinking. Troubleshoot multiple alarms

--- build/js/app.js
+++ build/js/app.js
@@ -25,48 +25,45 @@ function resetFields () {
   $("#am_pm").val("");
 }
 
+// moment() here is a function that has been installed through Bower and packaged in the vendor.js and vendor.css files
+// time variable should be independent of document or form submit status
+var time = moment().format('LT');
+var alarm = false;
+var setAlarm;
+
 $(document).ready(function() {
 
-  // moment() here is a function that has been installed through Bower and packaged in the vendor.js and vendor.css files
-  // time variable should be independent of document or form submit status
-  var time = moment().format('LT');
-
-  // QUESTION??? how can we update the time synchronously
-  setInterval(function() {
+  // show current time synchronously. update every half a second.
+  setInterval(function(){
     time = moment().format('LT');
-  }, 60000);
+    $("#currentTime").text(time);
+  }, 500);
 
-  //show current time
-  $("#currentTime").text(time);
-
-  // add alarm when document is loaded
-  $(".addAlarm").click(function() {
-    $("#alarm").append(
-      '<form class="alarm" action="index.html" method="post">' +
-        '<label for="hour">Hour</label>' +
-        '<input type="text" name="hour" id="hour">' +
-        '<label for="minute">Minute</label>' +
-        '<input type="text" name="minute" id="minute">' +
-        '<label for="am_pm"></label>' +
-        '<select id="am_pm" name="am_pm">' +
-          '<option value=""></option>' +
-          '<option value="am">am</option>' +
-          '<option value="pm">pm</option>' +
-        '</select>' +
-        '<button type="submit" name="button">Set Alarm</button><br><br>' +
-        '<button class="btn btn-info" id="five" type="button">Snooze 5 mins</button>' +
-        '<button class="btn btn-success ten" id="ten" type="button">Snooze 10 mins</button>' +
-      '</form>'
-    );
-  });
-
-  // remove alarm when document is loaded
-  $(".removeAlarm").click(function() {
-    $(".alarm").last().remove();
-  });
-
-  var alarm = false;
-  var setAlarm;
+  // // add alarm when document is loaded
+  // $(".addAlarm").click(function() {
+  //   $("#alarm").append(
+  //     '<form class="alarm" action="index.html" method="post">' +
+  //       '<label for="hour">Hour</label>' +
+  //       '<input type="text" name="hour" id="hour">' +
+  //       '<label for="minute">Minute</label>' +
+  //       '<input type="text" name="minute" id="minute">' +
+  //       '<label for="am_pm"></label>' +
+  //       '<select id="am_pm" name="am_pm">' +
+  //         '<option value=""></option>' +
+  //         '<option value="am">am</option>' +
+  //         '<option value="pm">pm</option>' +
+  //       '</select>' +
+  //       '<button type="submit" name="button">Set Alarm</button><br><br>' +
+  //       '<button class="btn btn-info" id="five" type="button">Snooze 5 mins</button>' +
+  //       '<button class="btn btn-success ten" id="ten" type="button">Snooze 10 mins</button>' +
+  //     '</form>'
+  //   );
+  // });
+  //
+  // // remove alarm when document is loaded
+  // $(".removeAlarm").click(function() {
+  //   $(".alarm").last().remove();
+  // });
 
   // what to do on submit
   $("#alarm").submit(function(event) {
@@ -82,31 +79,43 @@ $(document).ready(function() {
     // display alarm set time
     $("#alarmTime").text(setAlarm);
 
-    // resetFields so new alarm can be set easily
+    // resetFields after parameter transfer so new alarm can be set easily
     resetFields();
 
   // end of submit event
   });
 
-  // snooze 5 --continuos
-  $("#five").click(function() {
-    alarm = false;
-    setInterval(alert = true, 2000);
-  });
+  // set alarm to true and display whenever time = setAlarm --check every half a second
+  setInterval(function(){
+    if (time == setAlarm) {
 
-  // snooze 10 --continuos
-  $("#ten").click(function() {
-    alarm = false;
-    // alarm.delay(600000);
-  });
+      // snooze 5min while alarm is true -- set alarm to true after 5 mins
+      $("#five").click(function() {
+        alarm = false;
+        $("#ring").text("");
+        setInterval(function(){
+            alarm = true;
+            $("#ring").text("ALARM!!!!");
+        }, 300000);
+      });
 
-  // set alarm to true and display whenever time = setAlarm --continuos
-  if (time === setAlarm) {
-    alarm = true;
-    $("#ring").text("ALARM!!!!");
-  } else {
-    $("#ring").text("");
-  }
+      // snooze 10 while alarm is true --continuos
+      $("#ten").click(function() {
+        alarm = false;
+        setInterval(function(){
+            alarm = true;
+            $("#ring").text("ALARM!!!!");
+        }, 600000);
+      });
+
+      alarm = true;
+      $("#ring").text("ALARM!!!!");
+
+    } else {
+      alarm = false;
+      $("#ring").text("");
+    }
+  }, 50);
 
 // end of document ready
 });
